cab50811e98203653450d71cc453b49c42f0545f
simon <simon@DESKTOP-TE8R63E>
2020-08-28 07:59:24 +0200

    prima Commit - creazione del progetto

---
+++ src/main/java/it/fiani/sondaggi/SondaggiApplication.java
@@ -0,0 +1,13 @@
+package it.fiani.sondaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fiani/sondaggi/SondaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fiani.sondaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
